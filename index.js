317d4fc4200a0a6b0037465415f2819392debe04
faethflex <gfletchjr@gmail.com>
2017-05-30 21:36:30 -0400

    finished lab, but 'undefined' shows up before movies list. small bug

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ var data_json;
 //request.open('GET', 'http://jsonplaceholder.typicode.com/posts/');
 request.open('GET', 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD');
 
-request.onreadystatechange = function(){
+request.onreadystatechange = function()
+{
 
-	if((request.status==200) && (request.readyState==4)){
+	if((request.status==200) && (request.readyState==4))
+  {
 		data_json = JSON.parse(request.response);
 
-		for(var i in data_json){
+		for (var i in data_json)
+    {
 			var data = data_json[i];
+      for (var j = 0; j < data.length; j++)
+      {
+        if (data[j][10] === "Golden Gate Bridge")
+        {
+    			var title = "<h3>" + data[j]["8"] + "</h3>";
+    			var releaseYear = "<p>Year of Release: " + data[j]["9"] + "</p>";
+          var productionCompany = "<p>Production Company: " + data[j]["13"] + "</p>";
 
-			console.log(data_json);
-			output = "<h3>" + data["view", "attribution"] + "</h3>";
-			output += "<p>" + data["view", "name"] + "</p>";
-      output += "<p>" + data["i"].production_company + "</p>";
-		}
+          output += title + "<br />" + releaseYear + "<br />" + productionCompany + "<br />"
+        }
+		  }
+    }
 
-		document.getElementById("result").innerHTML"<br />" += output
+		document.getElementById("result").innerHTML = output
 
 	}
 }
