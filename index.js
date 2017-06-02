3086742ec436b7134af05d47b40fc1250c0d7899
Aurora Banuelos <aurorabanuelos@zipcoder5.local>
2017-06-02 00:28:02 -0400

    completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = new XMLHttpRequest();
-var output;
 var data_json;
+var output = "";
 
 request.open('GET', 'https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD');
 
@@ -12,16 +12,12 @@ request.onreadystatechange = function(){
     if((request.status==200) && (request.readyState == 4)){
         
         data_json = JSON.parse(request.response);
-        console.log(data_json);
 
 		for(var i = 0; i < data_json.data.length; i++){
-			//var data = data_json.data[i];
             
             if(data_json.data[i][10] == "Golden Gate Bridge"){
-                output = "<h3>" + data_json.data[i][8] + "</h3>" + data_json.data[i][9] + data_json.data[i][12];
+                output += "<h3>" + data_json.data[i][8] + "</h3>" + "Released: " +data_json.data[i][9] + " Production Company: " +data_json.data[i][12];
             }
-
-			
 
 		}
 
